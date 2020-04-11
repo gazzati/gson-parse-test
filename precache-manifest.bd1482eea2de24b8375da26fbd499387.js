83e07d76dacd6e95c2a35c061ba7f81ea3b9bc09
Timur <gazzaevtimur@gmail.com>
2020-04-11 23:10:58 +0300

    Updates

---
+++ precache-manifest.bd1482eea2de24b8375da26fbd499387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa70c82dff68a8a41c92f5a8f58eb7f",
+    "url": "/gsonParse-test/index.html"
+  },
+  {
+    "revision": "647fe0ded9d62128fd99",
+    "url": "/gsonParse-test/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "12ab51d2ab96d08d3e63",
+    "url": "/gsonParse-test/static/css/main.8ac8d0c5.chunk.css"
+  },
+  {
+    "revision": "647fe0ded9d62128fd99",
+    "url": "/gsonParse-test/static/js/2.48aa9ab2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/gsonParse-test/static/js/2.48aa9ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ab51d2ab96d08d3e63",
+    "url": "/gsonParse-test/static/js/main.1c7bdc17.chunk.js"
+  },
+  {
+    "revision": "1d6ff040fad4022b5e4f",
+    "url": "/gsonParse-test/static/js/runtime-main.713080f1.js"
+  }
+]);
